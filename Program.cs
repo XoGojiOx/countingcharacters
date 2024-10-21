8ab40334f4fba864896989fdc069af5d4362e120
XoGojiOx <xogojiox@gmail.com>
2024-10-20 20:33:23 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine().ToLower();
+
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c))
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+        }
+
+        foreach (var item in charCount)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
